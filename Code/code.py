26955282d65e02c542fb9a34c8f856787ae19e39
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-03-11 15:33:55 +0200

    Read and Display Image

--- Code/code.py
+++ Code/code.py
@@ -1,2 +1,15 @@
 import cv2
 
+# File Path to Image
+image = cv2.imread('../Images/image1.jpg', 0)
+# Set Up Window
+cv2.imshow('Digital Image Processing', image)
+
+key = cv2.waitKey(0)
+# Exit Keys (Esc - Quit) (S - Save and Quit)
+if key == 27:
+    cv2.destroyAllWindows()
+elif key == ord('s'):
+    cv2.imwrite('../Images/saved_image.png', image)
+    cv2.destroyAllWindows()
+
