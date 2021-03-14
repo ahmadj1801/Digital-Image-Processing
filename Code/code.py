22dd4dc5dcc075d167ea54bf28d649c35e061199
Ahmadj1801 <ahmadj1801@gmail.com>
2021-03-14 15:06:49 +0000

    Command Line arguements

--- Code/code.py
+++ Code/code.py
@@ -1,7 +1,17 @@
+import argparse as ap
 import cv2
 
+# python code.py -i image.png
+
+# Set Up parameters from console
+parser = ap.ArgumentParser()
+parser.add_argument('-i', '--image', required=True, help='Insert Image Name')
+args = vars(parser.parse_args())
+image_path = args['image']
+image_path = '../Images/'+image_path
+
 # File Path to Image
-image = cv2.imread('../Images/image1.jpg', 0)
+image = cv2.imread(image_path, 0)
 # Set Up Window
 cv2.imshow('Digital Image Processing', image)
 
