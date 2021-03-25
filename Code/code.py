eebc6a5f8e0acbe2e802b2728036d72d8a10f17c
Ahmad Jawaad Shah <ahmadj1801@gmail.com>
2021-03-25 19:04:35 +0200

    Video Looping [Done] Colour Extraction [Done]

--- Code/code.py
+++ Code/code.py
@@ -2,25 +2,46 @@ import argparse as ap
 import cv2
 import sys
 
-media_path = '../Images/maradona.jpg'
-window = cv2.VideoCapture(media_path)
 
-if not window.isOpened():
-    print('Failed To Open!')
-    sys.exit()
+def click(event, x, y, flags, param):
+    global image
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        blue, green, red = image[y, x]
+        print('Colours at [', x, ', ', y, '] = < ', red, ', ', green, ', ', blue, ' >')
 
-while window.isOpened():
-    ret, frame = window.read()
-    if ret:
-        img = cv2.imshow('Digital Image Processing', frame)
-    else:
-        window.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+media_path = '../Images/graph.gif'
+extension = media_path[-3::]
+video_formats = ['gif', 'mp4']
 
-window.release()
+if extension not in video_formats:
+    cv2.namedWindow("Digital Image Processing")
+    cv2.setMouseCallback("Digital Image Processing", click)
+    image = cv2.imread(media_path)
+    while True:
+        cv2.imshow("Digital Image Processing", image)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+else:
+    window = cv2.VideoCapture(media_path)
+    if not window.isOpened():
+        print('Failed To Open!')
+        sys.exit()
+
+    while window.isOpened():
+        ret, frame = window.read()
+        if ret:
+            img = cv2.imshow('Digital Image Processing', frame)
+
+        else:
+            window.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            window.release()
+            break
+
 cv2.destroyAllWindows()
 sys.exit()
 
